c728dbee6c40cff7a908a18909a55b4117611680
Anderson <anderson.simioni22@gmail.com>
2025-06-23 23:08:07 -0300

    fixes

--- Program.cs
+++ Program.cs
@@ -8,10 +8,16 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite("Data Source=users.db"));
+    options.UseSqlite("Data Source=projetoweb.db"));
 
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated(); // cria o banco e as tabelas se nao existirem
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
